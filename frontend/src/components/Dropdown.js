838f9942cee0f203bc09417939081ca20f345f30
ben-wsk9531 <wsk9531@autuni.ac.nz>
2021-10-14 19:20:50 +1300

    added table

---
+++ frontend/src/components/Dropdown.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SEPractices from "../dumydata/SEPractices";
+
+const optionItems = SEPractices.map((SEPractice) => (
+  <option key={SEPractice.practice}>{SEPractice.practice}</option>
+));
+const Dropdown = () => {
+  return (
+    <div>
+      <select>{optionItems}</select>
+    </div>
+  );
+};
+export default Dropdown;
--- frontend/src/components/SEPractice.js
+++ frontend/src/components/SEPractice.js
@@ -1,17 +1,22 @@
 import React from "react";
+import articles from "../dumydata/articles.js";
+import Styles from "../components/tablestyle";
+import Table from "../components/Evidencetable";
+import tablecolumns from "./tablecolumns";
+import Dropdown from "./Dropdown.js";
 
 const SEPractice = () => {
   return (
     <div>
       <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-      <p>
-        There will be a way to select an SE practice from our SEPER repository
-        here
-      </p>
-      <p>
-        Also the display table of evidence levels for different claims will show
-        here for the SE practice selected
-      </p>
+      <Dropdown>
+        <Styles>
+          <Table>
+            data={articles}
+            columns={tablecolumns}
+          </Table>
+        </Styles>
+      </Dropdown>
     </div>
   );
 };
